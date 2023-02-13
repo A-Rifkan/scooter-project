c3865138796a18f348a52ad57260470660b94280
A-Rifkan <121014412+A-Rifkan@users.noreply.github.com>
2023-02-13 05:52:36 +0000

    Scooter test

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,54 @@
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
-//typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+describe('Scooter', () => {
+  let scooter;
+  beforeEach(() => {
+    scooter = new Scooter('station', 'user', 50, false);
+  });
 
-//Method tests
-describe('scooter methods', () => {
-  // tests here!
+  test('rent method should update station to null and user to station1 if scooter is charged and not broken', () => {
+    scooter.rent();
+    expect(scooter.station).toBe(null);
+    expect(scooter.user).toBe('station');
+  });
 
-  //rent method
+  test('rent method should throw an error if scooter is not charged', () => {
+    scooter.charge = 10;
+    expect(() => {
+      scooter.rent();
+    }).toThrowError('scooter needs to charge');
+  });
 
-  //dock method
+  test('rent method should throw an error if scooter is broken', () => {
+    scooter.isBroken = true;
+    expect(() => {
+      scooter.rent();
+    }).toThrowError('scooter needs to be repaired');
+  });
 
-  //requestRepair method
+  test('dock method should update station to station2 and user to null', () => {
+    scooter.dock('station');
+    expect(scooter.station).toBe('station');
+    expect(scooter.user).toBe(null);
+  });
 
-  //charge method
+  test('recharge method should increase charge of scooter by 1 every second', () => {
+    jest.useFakeTimers();
+    scooter.recharge();
+    expect(scooter.charge).toBe(50);
+    jest.advanceTimersByTime(1000);
+    expect(scooter.charge).toBe(51);
+    jest.runAllTimers();
+    expect(scooter.charge).toBe(100);
+  });
 
-})
+  test('requestRepair method should change isBroken to false', () => {
+    const scooter = new Scooter("A Station", "A User", 50, true);
+    scooter.requestRepair();
+  
+    setTimeout(() => {
+      expect(scooter.isBroken).toBe(false);
+    }, 5100);
+  });
+});
